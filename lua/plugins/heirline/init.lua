73f29c3955fc4e46aac5f2a1bd6f231e61ce0408
Broken-Deer <64290747+Broken-Deer@users.noreply.github.com>
2025-07-20 01:05:01 +0800

    tabline use bufferline

--- lua/plugins/heirline/init.lua
+++ lua/plugins/heirline/init.lua
@@ -1,14 +1,15 @@
 vim.o.laststatus = 3
-
+-- vim.o.showtabline = 2
+vim.cmd([[au FileType * if index(['wipe', 'delete'], &bufhidden) >= 0 | set nobuflisted | endif]])
 return {
   "rebelot/heirline.nvim",
   event = "UiEnter",
   config = function()
-    local utils = require("heirline.utils")
     local statuslines = require("plugins.heirline.statusline")
     -- TODO: TerminalStatusline, HelpPageStatusline,DAPStatusline, split into many files
     require("heirline").setup({
       statusline = { fallthrough = false, statuslines.terminal, statuslines.default },
+      -- tabline = require("plugins.heirline.tabline"),
     })
   end,
 }
