52042176106d86ff759e8ed1869837ef792b70fc
Broken-Deer <64290747+Broken-Deer@users.noreply.github.com>
2025-07-21 00:49:57 +0800

    feat: session manager use `persistence.nvim`

---
+++ lua/plugins/persistence.lua
@@ -0,0 +1,37 @@
+return {
+  "folke/persistence.nvim",
+  event = "BufReadPre",
+  opts = {
+    need = 2,
+  },
+  keys = {
+    {
+      "<leader>Ss",
+      function()
+        require("persistence").select()
+      end,
+      desc = "Select a session to load",
+    },
+    {
+      "<leader>Sl",
+      function()
+        require("persistence").load({ last = true })
+      end,
+      desc = "Load the last session",
+    },
+    {
+      "<leader>Sc",
+      function()
+        require("persistence").load()
+      end,
+      desc = "Load the session for the current directory",
+    },
+    {
+      "<leader>Sd",
+      function()
+        require("persistence").stop()
+      end,
+      desc = "Stop persistence",
+    },
+  },
+}
