5107da7ae95257ab6a54d4d0d3425466e696795a
Broken-Deer <64290747+Broken-Deer@users.noreply.github.com>
2025-07-20 17:01:43 +0800

    chore: aerial keymaps

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -1,15 +1,20 @@
 return {
   "stevearc/aerial.nvim",
-  opts = {},
+  event = "VeryLazy",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
-  keys = {
-    {
-      "<leader>a",
-      "<cmd>AerialToggle<cr>",
-      desc = "Open or close aerial",
-    },
-  },
+  config = function()
+    require("aerial").setup({
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "<c-u>", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "<c-d>", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    })
+    -- You probably also want to set a keymap to toggle aerial
+    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>", { desc = "Open or close aerial" })
+  end,
 }
